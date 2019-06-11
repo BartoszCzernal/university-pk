73d916e7c84e3b3797d62510d3be77d5a7c5fef0
BartoszCzernal <bartekczernal@gmail.com>
2019-06-11 20:39:02 +0200

    Stworzenie projektow Sklep_6_1 i Sklep_6SE

---
+++ lab1/Sklep_6_1/src/java/warstwa_biznesowa/Fasada_warstwy_biznesowej_ejb.java
@@ -0,0 +1,23 @@
+package warstwa_biznesowa;
+
+import java.util.ArrayList;
+import java.util.Date;
+import javax.ejb.Stateless;
+
+@Stateless
+public class Fasada_warstwy_biznesowej_ejb {
+   
+    Fasada_warstwy_biznesowej fasada = new Fasada_warstwy_biznesowej();
+    
+    public void utworz_produkt(String dane[], Date data) {
+        fasada.utworz_produkt(dane, data);
+    }
+    
+    public String[] dane_produktu() {
+        return fasada.dane_produktu();
+    }
+    
+    public ArrayList<ArrayList<String>> items() {
+        return fasada.items();
+    }
+}
